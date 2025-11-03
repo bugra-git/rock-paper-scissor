1cd2985727baec970b2b2aa840b480ae6f4b49b3
bugra-git <237065860+bugra-git@users.noreply.github.com>
2025-11-03 18:03:43 +0300

    Create game function

--- gamePaperRock.js
+++ gamePaperRock.js
@@ -13,7 +13,6 @@ function getComputerChoice() {
     else getComputerChoice();
     return computerChoice;
 }
-let computerChoice = getComputerChoice();
 
 function getHumanChoice() {
     let choice;
@@ -27,4 +26,52 @@ function getHumanChoice() {
     } else getHumanChoice();
     return choice;
 }
-let humanChoice = getHumanChoice();
+
+function score() {
+    let scoreBoard = `computer score:${computerScore}-human score:${humanScore}`;
+    console.log(scoreBoard);
+}
+let computerScore = 0;
+let humanScore = 0;
+score();
+
+function play() {
+    let message;
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    if (computerChoice === humanChoice) {
+        message = "You draw";
+    } else if (humanChoice === "Rock") {
+        message = (computerChoice === "Paper")? "You lost, computer had paper" : "You won, computer had scissors";
+    } else if (humanChoice === "Paper") {
+        message = (computerChoice === "Scissors")? "You lost, commputer had scissors" : "You won, computer had rock";
+    } else if (humanChoice === "Scissors") {
+        message = (computerChoice === "Rock")? "You lost, computer had rock" : "You won, computer had paper";
+    }
+    console.log(message);
+    if (message.slice(0,7) === "You won") {
+        humanScore++;
+    } else if (message.slice(0,8) === "You lost") {
+        computerScore++;
+    }
+}
+
+function game() {
+    play();
+    score();
+    play();
+    score();
+    play();
+    score();
+    play();
+    score();
+    play();
+    score();
+    if (computerScore < humanScore) {
+        console.log("You won the game");
+    } else if (humanScore < computerScore) {
+        console.log("You lost the game");
+    } else (console.log("Game end in draw"))
+}
+
+game();
